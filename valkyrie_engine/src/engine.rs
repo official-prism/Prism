dca07f844207f7074658634048d904d025b52ddc
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2026-07-08 15:09:00 +0200

    Copyright adjustment

--- valkyrie_engine/src/engine.rs
+++ valkyrie_engine/src/engine.rs
@@ -1,7 +1,7 @@
 /*
     This file is part of Valkyrie.
 
-    Copyright (C) 2026 Tomasz Jaworski
+    Copyright (C) 2026 Tomasz Jaworski and Valkyrie Contributors
 
     Valkyrie is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,16 +14,24 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Valkyrie.  If not, see <https://www.gnu.org/licenses/>.
+    along with Valkyrie.  If not, see <https://gnu.org>.
 
-    Additional permission under GNU GPL version 3 section 7
+    Additional terms under GNU GPL version 3 section 7
 
+    1. Linking Exception:
     If you modify this Program, or any covered work, by linking or
     combining it with the ONNX Runtime and/or NVIDIA TensorRT libraries
     (or a modified version of those libraries), containing parts covered
     by the terms of the respective ONNX Runtime and NVIDIA license
     agreements, the licensors of this Program grant you additional
     permission to convey the resulting work.
+
+   2. Attribution Requirement (Section 7b):
+    Under Section 7(b) of the GNU General Public License, version 3,
+    the licensors of this Program require that any modified or redistributed
+    versions of this software must prominently preserve original author
+    attributions and credit to the Valkyrie Contributors in the software
+    documentation.
 */
 
 pub mod builder;
